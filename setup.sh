764f0900ef220169421d70a211741df0ec68f3bc
Varun Sundar <varun19299@gmail.com>
2018-04-19 15:25:22 +0530

    Updated Experimments 6,7

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@ echo "Running all scripts"
 bash 1-BasicSetUp.sh
 bash 2-AnacondaCustomization.sh
 bash 3-ProgTools.sh
+
+echo "The script has finished. The System will now reboot so that certain shell changes can take place"
+echo "sudo reboot"
+read -p "Press [Enter] to continue..." temp
+
+sudo reboot
